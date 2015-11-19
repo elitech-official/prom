334ec2a4cc098a35509fe6f49fdc746d778844c9
elitech-official <completelydifferentuser@gmail.com>
2015-11-19 09:07:31 +0200

    shcema

--- db/schema.rb
+++ db/schema.rb
@@ -14,30 +14,30 @@
 ActiveRecord::Schema.define(version: 20151118130709) do
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "image_path", limit: 255
+    t.string   "name",       limit: 191
+    t.string   "image_path", limit: 191
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
 
   create_table "companies", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.string   "phone",              limit: 255
-    t.string   "website",            limit: 255
-    t.string   "address",            limit: 255
+    t.string   "name",               limit: 191
+    t.string   "phone",              limit: 191
+    t.string   "website",            limit: 191
+    t.string   "address",            limit: 191
     t.integer  "budget",             limit: 4
     t.integer  "employee_count",     limit: 4
     t.integer  "subcategory_id",     limit: 4
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
-    t.string   "image_path",         limit: 255
+    t.string   "image_path",         limit: 191
     t.text     "description",        limit: 65535
-    t.string   "country",            limit: 255
+    t.string   "country",            limit: 191
     t.integer  "year_created",       limit: 4
-    t.string   "comp_type",          limit: 255
-    t.string   "business",           limit: 255
-    t.string   "mobile_phone",       limit: 255
-    t.string   "website_image_path", limit: 255
+    t.string   "comp_type",          limit: 191
+    t.string   "business",           limit: 191
+    t.string   "mobile_phone",       limit: 191
+    t.string   "website_image_path", limit: 191
   end
 
   create_table "pages", force: :cascade do |t|
@@ -46,24 +46,24 @@ ActiveRecord::Schema.define(version: 20151118130709) do
   end
 
   create_table "subcategories", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name",        limit: 191
     t.integer  "category_id", limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.string   "image_path",  limit: 255
+    t.string   "image_path",  limit: 191
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  limit: 191, default: "", null: false
+    t.string   "encrypted_password",     limit: 191, default: "", null: false
+    t.string   "reset_password_token",   limit: 191
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip",     limit: 191
+    t.string   "last_sign_in_ip",        limit: 191
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
