b7d9f691c75aa9eb29a1360f5bc6dc109761d7dc
elitech-official <completelydifferentuser@gmail.com>
2015-12-02 23:26:01 +0200

    blog & some more

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,8 +2,7 @@ class Blog < ActiveRecord::Base
   
   has_one :blog_image
   has_attached_file :thumb,
-    styles: { medium: "300x300>", large: "800x600>" },
-    default_url: "/images/missing.jpg"
+    styles: { medium: "300x300>", large: "800x600>" }
   validates_attachment_content_type :thumb, content_type: /\Aimage\/.*\Z/  
     
 end
